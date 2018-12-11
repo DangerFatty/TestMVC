5bb41d6920b5c88864b489288af246b0774d9f9b
DangerFatty <k23671254@gmail.com>
2018-12-11 17:30:44 +0800

    finished in 2018.12.11

--- WebApplication1/Controllers/TestController.cs
+++ WebApplication1/Controllers/TestController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class TestController : Controller
     {
-        // GET: Test
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Test()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Test(Test t)
+        {
+            double h = t.Height / 100;
+            double w = t.Weight;
+            t.BMI = w / h / h;
+
+            return View(t);
         }
     }
 }
--- WebApplication1/Models/Test.cs
+++ WebApplication1/Models/Test.cs
@@ -16,5 +16,11 @@ namespace WebApplication1.Models
         public DateTime Birthday { get; set; }
 
         public int Age { get; set; }
+
+        public double Height { get; set; }
+
+        public double Weight { get; set; }
+
+        public double BMI { get; set; }
     }
 }
